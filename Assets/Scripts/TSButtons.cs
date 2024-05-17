e68fa6a1e32e2a08aa3846161d3e31e3e50b14df
rmereditIGDA <rmeredit@uno.edu>
2024-05-17 14:10:54 -0500

    Title, Credits

---
+++ Assets/Scripts/TSButtons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TSButtons : MonoBehaviour
+{
+	public void NewGame() {
+		SceneManager.LoadScene("TestScene");
+	}
+	
+	public void Credits() {
+		SceneManager.LoadScene("CreditsScene");
+	}
+	
+	public void Return() {
+		SceneManager.LoadScene("TitleScene");
+	}
+	
+	public void QuitGame() {
+		Application.Quit();
+		// Debug.Log("Game is exiting");
+		// Just to make sure its working
+	}
+}
