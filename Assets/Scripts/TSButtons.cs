90ac7a12219f5308528e545dd7732d0d93252484
rmereditIGDA <rmeredit@uno.edu>
2024-05-17 21:40:15 -0500

    added music, art to titlescene. Lore screen uses global variable to support beg/mid/end scenes

--- Assets/Scripts/TSButtons.cs
+++ Assets/Scripts/TSButtons.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 public class TSButtons : MonoBehaviour
 {
 	public void NewGame() {
+		GlobalVariables.Set("lorePath", "Assets/Scripts/Dialogue/DialogueLore1.txt");
+		GlobalVariables.Set("nextScene", "DanielScene 1");
+		SceneManager.LoadScene("LoreScene1");
+	}
+	
+	public void Continue() {
+		GlobalVariables.Set("lorePath", "Assets/Scripts/Dialogue/DialogueLore2.txt");
+		GlobalVariables.Set("nextScene", "Zach's Scene");
 		SceneManager.LoadScene("LoreScene1");
 	}
 	
